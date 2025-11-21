f712e361bbfd5df5468239974429211ee8d8cc7d
6Morpheus6 <wanton-decay@gmx.net>
2025-11-21 19:52:06 +0100

    update
    fix rocm version

--- torch.js
+++ torch.js
@@ -67,7 +67,7 @@ module.exports = {
       "params": {
         "venv": "{{args && args.venv ? args.venv : null}}",
         "path": "{{args && args.path ? args.path : '.'}}",
-        "message": "uv pip install torch==2.7.0 torchvision==0.22.0 --index-url https://download.pytorch.org/whl/rocm6.1"
+        "message": "uv pip install torch==2.7.0 torchvision==0.22.0 --index-url https://download.pytorch.org/whl/rocm6.3"
       }
     },
     // linux cpu
